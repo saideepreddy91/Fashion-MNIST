8e7ad8dafc711643eeebdc821aa22fe78c420459
saideep <saideep@truenorthco.in>
2018-12-14 15:34:17 -0800

    config added

---
+++ config.py
@@ -0,0 +1,16 @@
+########
+# Config for VGG
+########
+
+vgg_model_path='assets/VGG16_aug_best_model_after_8epochs.pt'
+data_path='data/downloads'
+
+########
+# Config for 2 Conv & 3 FC with BN + Dropout
+########
+
+model_path = 'models/'
+
+#########
+#
+#########
